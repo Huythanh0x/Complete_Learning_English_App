aa89a4702ca72d08c093da5d19b2cf104fba3400
huythanh0x <huythanh0x@gmail.com>
2021-11-23 18:30:52 +0700

    add instruction ui

---
+++ app/src/main/java/com/batdaulaptrinh/completlearningenglishapp/ui/introduction/IntroductionActivity.kt
@@ -0,0 +1,12 @@
+package com.batdaulaptrinh.completlearningenglishapp.ui.introduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.batdaulaptrinh.completlearningenglishapp.R
+
+class IntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
